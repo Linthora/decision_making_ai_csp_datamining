a0f1c75372857ff856cbb2381f34db586808ab47
Felix <senechalfelix@outlook.fr>
2022-11-10 16:45:08 +0100

    apriori doc p1

--- src/datamining/Apriori.java
+++ src/datamining/Apriori.java
@@ -3,12 +3,37 @@ package datamining;
 import java.util.*;
 import representation.*;
 
-public class Apriori extends AbstractItemsetMiner {
+/**
+ * A class which represents extractors working on the principle of the "apriori" algorithm.
+ * extends {@link datamining.AbstractItemsetMiner}.
+ *
+ * The "apriori" algorithm is a method for finding frequent itemsets in a database.
+ * It is based on the principle that if an itemset is frequent, then all its subsets are frequent.
+ * It is a divide and conquer algorithm, which is composed of two phases:
+ * - the first phase generates all the frequent itemsets of size 1, then all the frequent itemsets of size 2, and so on.
+ * - the second phase generates all the frequent itemsets of size k+1 from the frequent itemsets of size k.
+ *
+ * The algorithm is based on the following two properties:
+ * - If an itemset is frequent, then all its subsets of size k-1 are frequent.
+ * - If an itemset is not frequent, then none of its supersets of size k+1 are frequent.
+ *
+ * For each pair of itemsets it combines them to give rise to the itemset of their union.
+ */
 
+public class Apriori extends AbstractItemsetMiner {
+    
+    /**
+     * Creates a new Apriori object.
+     * @param database the database to use herited from {@link datamining.AbstractItemsetMiner}.
+     */
     public Apriori(BooleanDatabase base) {
         super(base);
     }
-
+    
+    /**
+     * @param minFreq the minimum frequency of the itemsets to extract.
+     * @return the set of all singleton items (with their frequency) which frequently in the database is at least equal to the given one.
+     */
     public Set<Itemset> frequentSingletons(float minFreq) {
         Set<Itemset> okSingleton = new HashSet<>();
 
@@ -24,6 +49,12 @@ public class Apriori extends AbstractItemsetMiner {
         return okSingleton;
     }
 
+    /**
+     * 
+     * @param s1 the first SortedSet.
+     * @param s2 the second SortedSet.
+     * @return a SortedSet of items which is the union of the first and the last item of the second.
+     */
     public static SortedSet<BooleanVariable> combine(SortedSet<BooleanVariable> s1, SortedSet<BooleanVariable> s2) {
         
         if(s1.size() == 0 || s1.size() != s2.size() || s1.last().equals(s2.last()) || !s1.headSet(s1.last()).equals(s2.headSet(s2.last()))) {
@@ -36,6 +67,7 @@ public class Apriori extends AbstractItemsetMiner {
 
         return res; 
     }
+    
 
     public static boolean allSubsetsFrequent(Set<BooleanVariable> items, Collection<SortedSet<BooleanVariable>> frequentsItems) {
         SortedSet<BooleanVariable> tempo = new TreeSet<>(COMPARATOR);
